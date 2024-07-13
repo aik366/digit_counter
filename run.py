0a224f29b248a775b6d722fde19418e6539e9ba6
Hayk <haykooo3@gmail.com>
2024-07-13 17:49:35 +0300

    commit 2

--- run.py
+++ run.py
@@ -16,17 +16,17 @@ class App(ctk.CTk):
         self.button1 = ctk.CTkButton(self, text="Увеличение", width=220, height=50, corner_radius=25, border_width=2,
                                      font=("Roboto", 17), fg_color="#4169E1", hover_color='#6495ED',
                                      command=self.counter_digits)
-        self.button1.pack(padx=20, pady=10)
+        self.button1.pack(padx=20, pady=20)
 
         self.button2 = ctk.CTkButton(self, text="Перезагрузить", width=220, height=50, corner_radius=25, border_width=2,
                                      font=("Roboto", 17), fg_color="#800000", hover_color='#A52A2A',
                                      command=self.label_close)
-        self.button2.pack(padx=20, pady=10)
+        self.button2.pack(padx=20, pady=(0, 20))
         self.button2.pack_forget()
 
     def counter_digits(self):
         self.label.configure(text=str(int(self.label.cget("text")) + 1))
-        self.button2.pack(padx=20, pady=10)
+        self.button2.pack(padx=20, pady=(0, 20))
 
     def label_close(self):
         self.label.configure(text="0")
