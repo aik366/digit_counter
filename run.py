91f46016da973c15e436c27c497668a17930b6a9
Hayk <haykooo3@gmail.com>
2024-07-13 17:46:30 +0300

    start commit

---
+++ run.py
@@ -0,0 +1,38 @@
+import customtkinter as ctk
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
+
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("+700+400")
+        self.title("Счётчик цифр")
+
+        self.label = ctk.CTkLabel(self, text="0", font=("Roboto", 50))
+        self.label.pack(padx=20, pady=50)
+
+        self.button1 = ctk.CTkButton(self, text="Увеличение", width=220, height=50, corner_radius=25, border_width=2,
+                                     font=("Roboto", 17), fg_color="#4169E1", hover_color='#6495ED',
+                                     command=self.counter_digits)
+        self.button1.pack(padx=20, pady=10)
+
+        self.button2 = ctk.CTkButton(self, text="Перезагрузить", width=220, height=50, corner_radius=25, border_width=2,
+                                     font=("Roboto", 17), fg_color="#800000", hover_color='#A52A2A',
+                                     command=self.label_close)
+        self.button2.pack(padx=20, pady=10)
+        self.button2.pack_forget()
+
+    def counter_digits(self):
+        self.label.configure(text=str(int(self.label.cget("text")) + 1))
+        self.button2.pack(padx=20, pady=10)
+
+    def label_close(self):
+        self.label.configure(text="0")
+        self.button2.pack_forget()
+
+
+if __name__ == '__main__':
+    app = App()
+    app.mainloop()
